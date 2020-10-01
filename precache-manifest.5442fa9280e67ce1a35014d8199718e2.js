2b91820ffc3f386b40f415035fa6891945bc1d87
wildkiomi <whisann.lie14@gmail.com>
2020-10-01 13:13:41 +0300

    Updates

---
+++ precache-manifest.5442fa9280e67ce1a35014d8199718e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8f3018f2084db63246cf467a88449a",
+    "url": "/movielib/index.html"
+  },
+  {
+    "revision": "e6ecf3e29afb06ed35ca",
+    "url": "/movielib/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e6ecf3e29afb06ed35ca",
+    "url": "/movielib/static/js/2.80fa5997.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movielib/static/js/2.80fa5997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7c60ee6fe5e43a5785",
+    "url": "/movielib/static/js/main.7ca6be3e.chunk.js"
+  },
+  {
+    "revision": "5d3a5f52b7a7e5e5cab8",
+    "url": "/movielib/static/js/runtime-main.eadb504b.js"
+  }
+]);
